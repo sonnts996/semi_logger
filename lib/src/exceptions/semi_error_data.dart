fa8aa536a8f8117745db558aaf0d9c528a6a7a8e
30367 <sonnt100@fpt.com.vn>
2022-01-20 10:19:51 +0700

    update semi_error_data.dart

--- lib/src/exceptions/semi_error_data.dart
+++ lib/src/exceptions/semi_error_data.dart
@@ -3,64 +3,68 @@
  Copyright (c) 2022 . All rights reserved.
 */
 
+/// Error code
+///
+class SemiErrorCode {
 // unknown
-const unknown = 'unknown';
+  static const unknown = 'unknown';
 
 // timeout
-const timeout = 'timeout';
+  static const timeout = 'timeout';
 
 // out of memory
-const outOfMemory = 'out-of-memory';
+  static const outOfMemory = 'out-of-memory';
 
 // permission denied
-const permissionDenied = 'permission-denied';
+  static const permissionDenied = 'permission-denied';
 
 // null data error
-const nullDataError = 'null-data-error';
+  static const nullDataError = 'null-data-error';
 
 // cast data error
-const castDataError = 'cast-data-error';
+  static const castDataError = 'cast-data-error';
 
 // index out of bounds
-const indexOutOfBounds = 'index-out-of-bounds';
+  static const indexOutOfBounds = 'index-out-of-bounds';
 
 // empty list
-const emptyList = 'empty-list';
+  static const emptyList = 'empty-list';
 
 // whitespace
-const whitespace = 'whitespace';
+  static const whitespace = 'whitespace';
 
 /* file and network error */
 
 // no internet access
-const noInternetAccess = 'no-internet-access';
+  static const noInternetAccess = 'no-internet-access';
 
 // access denied
-const accessDenied = 'access-denied';
+  static const accessDenied = 'access-denied';
 
 // connection fail
-const connectionFail = 'connection-fail';
+  static const connectionFail = 'connection-fail';
 
 //400
-const badRequest = 'bad-request';
+  static const badRequest = 'bad-request';
 
 //401
-const unauthorized = 'unauthorized';
+  static const unauthorized = 'unauthorized';
 
 //401
-const forbidden = 'forbidden';
+  static const forbidden = 'forbidden';
 
 //404
-const fileNotFound = 'file-not-found';
+  static const fileNotFound = 'file-not-found';
 
 //405
-const methodNotAllowed = 'methodNotAllowed';
+  static const methodNotAllowed = 'methodNotAllowed';
 
 // 408
-const requestTimeOut = 'request-timeout';
+  static const requestTimeOut = 'request-timeout';
 
 // 500
-const serverError = 'server-error';
+  static const serverError = 'server-error';
 
 // 501
-const badGateway = 'bad-gateway';
+  static const badGateway = 'bad-gateway';
+}
